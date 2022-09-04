0513baa68cf9d8e7bea84feaf9a3f3b09b29e395
Narayan <laxminarayanreddy432@gmail.com>
2022-09-04 09:57:28 +0530

    'commit_from_script'

---
+++ heap/KthLargestElementinAnArray.cpp
@@ -0,0 +1,18 @@
+//215. Kth Largest Element in an Array : leetcode
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        
+        priority_queue<int,vector<int>,greater<int> > min_heap;
+        
+        for(int i=0;i<nums.size();i++){
+            min_heap.push(nums[i]);
+            if(min_heap.size()>k){
+                min_heap.pop();
+            }
+        }
+        
+        return min_heap.top();
+        
+    }
+};
